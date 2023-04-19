812cb9b2ecdfe3a43083e1b209398c80eff3150e
codedbyshaun <shaun@codedbyshaun.com>
2023-04-19 16:31:29 +0200

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+
+  try {
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "c67dd7dc-ddf0-42c7-9974-a26c6d833f1c" }}
+    )
+    return res.status(r.status).json(r.data)
+  } catch (err) {
+    return res.status(err.response.status).json(err.response.data)
+  }
+  
+  return res.json({ username: username, secret: "auth567..." });
+});
+
+app.listen(3001);
